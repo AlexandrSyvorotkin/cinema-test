c4f540f629d91a2f9ea1b7146c70f82fded00f1f
AlexSyvorotkin <aleks031994@gmail.com>
2026-06-05 13:17:37 +0300

    update

--- src/data/characterMock.ts
+++ src/data/characterMock.ts
@@ -1,8 +1,8 @@
 import type { SearchMock } from '../types/searchMock'
-import photoPortrait from '../assets/Screenshot_1.png'
+import photoPortrait from '../assets/фото1.png'
 import photoCircus from '../assets/Screenshot_2.png'
 import photoGalleryStack from '../assets/дсп.png'
-import photoCircusVenue from '../assets/цирк.png'
+import photoCircusVenue from '../assets/фото2.png'
 
 export const characterMock: SearchMock = {
   variant: 'character',
